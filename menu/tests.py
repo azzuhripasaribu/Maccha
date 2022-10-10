e0ca2106a8817450df6d8f4491010de8db54d1a0
AjieRestu_ <ajierestu03@gmail.com>
2022-10-10 22:53:45 +0700

    Implement add menu backend feature

--- menu/tests.py
+++ menu/tests.py
@@ -1,5 +1,10 @@
+from tkinter import Menu
+from urllib import response
 from django.test import TestCase, Client
 from django.urls import reverse
+from account.models import Account
+from menu.models import menuModel
+
 
 class BaseTest(TestCase):
     def setUp(self):
@@ -7,6 +12,7 @@ class BaseTest(TestCase):
         self.register_url = reverse('register_page')
         self.login_url = reverse('login_page')
         self.menu_url = reverse('menu')
+        self.addmenu_url = reverse('add-menu')
         self.user = {
             'email': 'test@email.com',
             'full_name': 'Full Name',
@@ -14,6 +20,13 @@ class BaseTest(TestCase):
             'username': 'username',
             'password': 'password',
             'password_2': 'password'
+        }
+        self.menu = {
+            'id' : 1,
+            'user' : 'test@email.com',
+            'name' : 'ayam',
+            'price' : 50000,
+            'description' : 'hewan berbulu'
         }
         self.client.post(self.register_url, self.user, format='text/html')
 
@@ -27,4 +40,20 @@ class MenuTest(BaseTest):
         response = self.client.get(self.menu_url)
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response,"menu.html")
-           
+
+    def test_add_menu_get(self):
+        self.client.post(self.login_url,self.user,format='text/html')
+        response = self.client.get(self.addmenu_url)
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,"add-menu.html")
+    
+    def test_unauthenticated_access_add_menu(self):
+        response = self.client.get(self.addmenu_url)
+        self.assertEqual(response.status_code,302)
+    
+    def test_add_menu_post(self):
+        self.client.post(self.login_url,self.user,format='text/html')
+        response = self.client.post(self.addmenu_url,self.menu,format ='text/html')
+        dummy_menu = menuModel.objects.filter(name = 'ayam').first()
+        self.assertEqual(dummy_menu.price,self.menu["price"])
+        self.assertEqual(response.status_code,302)       
--- menu/urls.py
+++ menu/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from menu.views import home
+from menu.views import home,add_menu
 
 
 
 urlpatterns = [
     path('',home, name='menu'),
+    path('add-menu',add_menu, name='add-menu'),
 ]
