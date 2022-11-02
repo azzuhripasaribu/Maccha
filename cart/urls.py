453255bd5edf648f7d86eeba698de03db301e89e
Azzuhri Nauli Pasaribu <azzuhri.nauli@ui.ac.id>
2022-11-03 00:40:20 +0700

    [GREEN] implemented payment

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,12 @@
 from unicodedata import name
 from django.urls import path
-from .views import cart_index, add_to_cart, cart_list,payment
+from .views import cart_index, add_to_cart, cart_list,payment, payment_success
 urlpatterns = [
     path("",cart_index, name="index"),
     path("add_to_cart",add_to_cart, name="add_to_cart"),
     path("list",cart_list, name="cart"),
     path("payment",payment, name="payment"),
+    path("payment_success",payment_success, name="payment_success"),
 ]
+
+# THIS
