209a1f8621af3ce16718a2e5f92c7fe21b8ebe9d
AjieRestu_ <ajierestu03@gmail.com>
2022-11-02 21:21:52 +0700

    [GREEN] PBI-5 Implement Calculation for price and quantity

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,9 @@
 from unicodedata import name
 from django.urls import path
-from .views import cart_index, add_to_cart
+from .views import cart_index, add_to_cart, cart_list,payment
 urlpatterns = [
     path("",cart_index, name="index"),
     path("add_to_cart",add_to_cart, name="add_to_cart"),
+    path("list",cart_list, name="cart"),
+    path("payment",payment, name="payment"),
 ]
