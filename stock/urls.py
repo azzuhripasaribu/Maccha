203e3e387c5d70f2f2d74cba1ddb1fec0196ff37
Azzuhri Nauli Pasaribu <azzuhri.nauli@ui.ac.id>
2022-11-20 16:58:20 +0700

    [Green] Implement update stock

--- stock/urls.py
+++ stock/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from stock.views import add_stock
+from stock.views import add_stock,update_stock
 
 app_name = 'stock'
 
 urlpatterns = [
     path("add",add_stock, name="add_stock"),
+    path("update/<id>", update_stock, name="update_stock")
 ]
