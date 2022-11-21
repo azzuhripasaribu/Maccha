4180ba0da8eef2e9d05a9e7a5adb8a297566f86c
Azzuhri Nauli Pasaribu <azzuhri.nauli@ui.ac.id>
2022-11-21 19:03:49 +0700

    [Refactor] Fixed clean code issue

--- stock/models.py
+++ stock/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from menu.models import menuModel;
+from menu.models import menuModel
 
 class Stock(models.Model):
     name = models.CharField(max_length=255)
